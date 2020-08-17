a74544a811931a639e035fbd7b421a01fbf4105b
Alexa <alexakearns@outlook.com>
2020-08-17 10:55:46 +0100

    Add shapes arrays to js

--- app.js
+++ app.js
@@ -5,8 +5,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-btn')
   const width = 10
 
+  const lShape= [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
+  const  zShape = [
+    [width*2, width+1, width*2+1, width+2],
+    [0, width, width+1, width*2+1],
+    [width*2, width+1, width*2+1, width+2],
+    [0, width, width+1, width*2+1]
+  ]
+
+  const tShape = [
+    [width, 1, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width*2+1, width+2],
+    [width, 1, width+1, width*2+1]
+  ]
+
+  const sqShape = [
+    [0, width, 1, width+1],
+    [0, width, 1, width+1],
+    [0, width, 1, width+1],
+    [0, width, 1, width+1]
+  ]
+
+  const iShape = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
   
-
+  const theShapes = [lShape, zShape, tShape, sqShape, iShape]
 })
 
