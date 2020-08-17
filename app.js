ee1a4c7e3f886d823fe6a8dea37ee26bc9722420
Alexa <alexakearns@outlook.com>
2020-08-17 12:24:09 +0100

    Format js document Add startBtn event listener

--- app.js
+++ app.js
@@ -1,162 +1,205 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-btn')
-  const width = 10
-  let nextRandom = 0
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-btn");
+  const width = 10;
+  let nextRandom = 0;
+  let timerId;
 
-  const lShape= [
-    [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2+2],
-    [1, width+1, width*2+1, width*2],
-    [width, width*2, width*2+1, width*2+2]
-  ]
+  const lShape = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
-  const  zShape = [
-    [width*2, width+1, width*2+1, width+2],
-    [0, width, width+1, width*2+1],
-    [width*2, width+1, width*2+1, width+2],
-    [0, width, width+1, width*2+1]
-  ]
+  const zShape = [
+    [width * 2, width + 1, width * 2 + 1, width + 2],
+    [0, width, width + 1, width * 2 + 1],
+    [width * 2, width + 1, width * 2 + 1, width + 2],
+    [0, width, width + 1, width * 2 + 1],
+  ];
 
   const tShape = [
-    [width, 1, width+1, width+2],
-    [1, width+1, width+2, width*2+1],
-    [width, width+1, width*2+1, width+2],
-    [width, 1, width+1, width*2+1]
-  ]
+    [width, 1, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width * 2 + 1, width + 2],
+    [width, 1, width + 1, width * 2 + 1],
+  ];
 
   const sqShape = [
-    [0, width, 1, width+1],
-    [0, width, 1, width+1],
-    [0, width, 1, width+1],
-    [0, width, 1, width+1]
-  ]
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1],
+    [0, width, 1, width + 1],
+  ];
 
   const iShape = [
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3],
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3]
-  ]
-  
-  const theShapes = [lShape, zShape, tShape, sqShape, iShape]
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
 
-  let currentPosition = 4
-  let currentRotation = 0
+  const theShapes = [lShape, zShape, tShape, sqShape, iShape];
 
-  let random = Math.floor(Math.random()*theShapes.length)
+  let currentPosition = 4;
+  let currentRotation = 0;
 
-  let current = theShapes[random][currentRotation]
+  let random = Math.floor(Math.random() * theShapes.length);
+
+  let current = theShapes[random][currentRotation];
 
   // add shape to div
   function draw() {
-    current.forEach(index => {
-      squares[currentPosition + index].classList.add('shape')
-    })
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("shape");
+    });
   }
 
   // remove shape from div
   function undraw() {
-    current.forEach(index => {
-      squares[currentPosition + index].classList.remove('shape')
-    })
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("shape");
+    });
   }
 
   // make shape move down each second
-  timerId = setInterval(moveDown, 1000)
+  // timerId = setInterval(moveDown, 1000);
 
   // assign functions to keycodes
   function control(e) {
-    if(e.keyCode === 37) {
-      moveLeft()
+    if (e.keyCode === 37) {
+      moveLeft();
     } else if (e.keyCode === 38) {
-      rotate()
+      rotate();
     } else if (e.keyCode === 39) {
-      moveRight()
+      moveRight();
     } else if (e.keyCode === 40) {
-      moveDown()
+      moveDown();
     }
   }
 
-  document.addEventListener('keyup', control)
+  document.addEventListener("keyup", control);
 
   function moveDown() {
-    undraw()
-    currentPosition += width
-    draw()
-    freeze()
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
   }
 
   function freeze() {
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-      random = nextRandom
-      nextRandom = Math.floor(Math.random() * theShapes.length)
-      current = theShapes[random][currentRotation]
-      currentPosition = 4
-      draw()
-      displayShape()
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theShapes.length);
+      current = theShapes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+      displayShape();
     }
   }
 
   function moveLeft() {
-    undraw()
-    const atLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    undraw();
+    const atLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
 
-    if(!atLeftEdge) currentPosition -=1
+    if (!atLeftEdge) currentPosition -= 1;
 
-    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition += 1
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
     }
-    draw()
+    draw();
   }
 
   function moveRight() {
-    undraw()
-    const atRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    undraw();
+    const atRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
 
-    if(!atRightEdge) currentPosition += 1
+    if (!atRightEdge) currentPosition += 1;
 
-    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition -= 1
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
     }
-    draw()
+    draw();
   }
 
   function rotate() {
-    undraw()
-    currentRotation ++
+    undraw();
+    currentRotation++;
 
-    if(currentRotation === current.length) {
-      currentRotation = 0
+    if (currentRotation === current.length) {
+      currentRotation = 0;
     }
-    current = theShapes[random][currentRotation]
-    draw()
+    current = theShapes[random][currentRotation];
+    draw();
   }
 
-  const displaySquares = document.querySelectorAll('.mini-grid div')
-  const displayWidth = 4
-  let displayIndex = 0
-  
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  let displayIndex = 0;
 
   const shapeUpNext = [
-    [1, displayWidth+1, displayWidth*2+1, 2],
-    [displayWidth*2, displayWidth+1, displayWidth*2+1, displayWidth+2],
-    [displayWidth, 1, displayWidth+1, displayWidth+2],
-    [0, displayWidth, 1, displayWidth+1],
-    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]
-  ]
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+    [
+      displayWidth * 2,
+      displayWidth + 1,
+      displayWidth * 2 + 1,
+      displayWidth + 2,
+    ],
+    [displayWidth, 1, displayWidth + 1, displayWidth + 2],
+    [0, displayWidth, 1, displayWidth + 1],
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1],
+  ];
 
   function displayShape() {
-    displaySquares.forEach(square => {
-      square.classList.remove('shape')
-    })
-    shapeUpNext[nextRandom].forEach(index => {
-      displaySquares[displayIndex + index].classList.add('shape')
-    })
+    displaySquares.forEach((square) => {
+      square.classList.remove("shape");
+    });
+    shapeUpNext[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("shape");
+    });
   }
 
-})
+  startBtn.addEventListener("click", () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theShapes.length);
+      displayShape();
+    }
+  });
 
+  function addScore() {
+    for (let i = 0; i < 199; i +=width) {
+      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+    }
+  }
+
+
+
+
+});
